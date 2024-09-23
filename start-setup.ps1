07c34aa28511be3bb79128cb140270779d412b11
kenaroik <134440593+kenaroik@users.noreply.github.com>
2024-09-23 11:20:27 +0200

    Update start-setup.ps1

--- start-setup.ps1
+++ start-setup.ps1
@@ -1,1 +1,3 @@
 start-process "powershell.exe" -verb "runas" -arg @("-command", "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))") -wait
+
+start-process "choco" -verb "runas" -arg @("install", "-y", "git", "gitextensions","vscode", "vscode-powershell")
